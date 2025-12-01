49009887479eab867985968058ceb5095d38262c
miflomi <82845042+miflomi@users.noreply.github.com>
2025-11-30 23:40:22 -0300

    Create auth.js

---
+++ middlewares/auth.js
@@ -0,0 +1,25 @@
+import jsonwebtoken from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const validarJWT = (request, response, next ) => {
+    const token = request.headers.authorization;
+    if( !token){
+        response.status(401).json({msg: 'Falta el token'});
+    }
+    const jwt = token.split(' ')[1];
+    console.log({jwt});
+    jsonwebtoken.verify(jwt, SECRET_KEY, (error, decoded) => {
+        if (error) {
+            response.status(403).json({ msg: 'Token invalidooo'});
+        }
+        console.log({decoded});
+        request.body.userId = decoded.id;
+    })
+
+    next();
+}
+
+export { validarJWT };
