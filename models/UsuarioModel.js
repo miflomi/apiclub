588f731e8ec2536f3a475d4bd6c9c585434e5a7d
miflomi <82845042+miflomi@users.noreply.github.com>
2025-11-30 23:42:13 -0300

    Add files via upload

---
+++ models/UsuarioModel.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const esquema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, "Falta el campo nombre"]
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    clave: String,
+    tel: Number,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Usuario = mongoose.model('Usuario', esquema);
+
+export default Usuario;
